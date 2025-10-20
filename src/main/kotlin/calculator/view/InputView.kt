56a9a43858ce9743c18cba019332deb830354a49
Jo Eungyeong <joeungyeong23@gmail.com>
2025-10-20 23:23:49 +0900

    fix(input): 입력값 처리 방식 수정

--- src/main/kotlin/calculator/view/InputView.kt
+++ src/main/kotlin/calculator/view/InputView.kt
@@ -1,14 +1,9 @@
 package calculator.view
 
-import calculator.constant.Constants.ZERO
 import camp.nextstep.edu.missionutils.Console
 
 class InputView {
     fun readInput(): String {
-        val input = Console.readLine()
-        if (input.isEmpty()) {
-            return ZERO
-        }
-        return input
+        return Console.readLine()
     }
 }
